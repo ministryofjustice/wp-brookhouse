4409d58bc6cad5f66adcc19f88d8d7f8ca0f98f7
adam <adam.brown@digital.justice.gov.uk>
2020-02-19 17:18:48 +0000

    Remove duplicate phone number and homepage layout.

--- web/app/themes/brookhouse/content-page.php
+++ web/app/themes/brookhouse/content-page.php
@@ -1,24 +1,25 @@
 <?php
+
 /**
  * The template used for displaying page content in page.php
  *
  * @package brookhouse
  */
+
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-	
-
-        <div class="entry-content<?php if(get_the_title()=="About the Investigation") echo " faqs"; ?>">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'brookhouse' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-	<?php //edit_post_link( __( 'Edit', 'brookhouse' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
+    
+        <div class="entry-content<?php if (get_the_title() == "About the Investigation") {
+            echo " faqs";
+                                 } ?>">
+        <?php the_content(); ?>
+        <?php
+            wp_link_pages(array(
+                'before' => '<div class="page-links">' . __('Pages:', 'brookhouse'),
+                'after'  => '</div>',
+            ));
+            ?>
+    </div><!-- .entry-content -->
+    <?php //edit_post_link( __( 'Edit', 'brookhouse' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
 </article><!-- #post-## -->
--- web/app/themes/brookhouse/sidebar.php
+++ web/app/themes/brookhouse/sidebar.php
@@ -1,18 +1,12 @@
 <?php
+
 /**
  * The Sidebar containing the main widget areas.
  *
  * @package brookhouse
  */
+
 ?>
 <div id="secondary" class="widget-area">
-    <div class="call-cta">
-        <a href="tel:02076334149">
-            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/call-for-info.svg" alt="Call with information regarding the Brook House Investigation">
-        </a>
-        <a href="tel:02076334149">
-            020 7633 4149
-        </a>
-    </div>
     <?php wp_nav_menu(array('theme_location' => 'primary', 'depth' => 0)); ?>
 </div><!-- #secondary -->
