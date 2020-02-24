88443854f5686f68b33bea94af28e4a02e0bf3f8
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2020-02-24 14:44:57 +0000

    Lowercase slug

--- web/app/themes/brookhouse/functions.php
+++ web/app/themes/brookhouse/functions.php
@@ -389,7 +389,7 @@ function homesettings_option_pages()
         acf_add_options_page(array(
             'page_title' => __('Footer Settings'),
             'menu_title' => __('Footer Settings'),
-            'menu_slug' => 'Footer-settings',
+            'menu_slug' => 'footer-settings',
             'capability' => 'edit_posts',
             'redirect' => false
         ));
