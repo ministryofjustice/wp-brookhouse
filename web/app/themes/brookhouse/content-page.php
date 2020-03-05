87b730d4f38cd0461054928d7b5618a623f6cccf
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2020-03-05 16:25:08 +0000

    Class Fix

--- web/app/themes/brookhouse/content-page.php
+++ web/app/themes/brookhouse/content-page.php
@@ -10,7 +10,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-    <div class="entry-content>
+    <div class="entry-content">
         <h1><?= get_the_title() ?></h1>
         <?php
         the_content();
