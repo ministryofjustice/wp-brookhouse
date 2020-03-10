f8b31a39b05fae07bf6528fe5b926ebe709a532c
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2020-03-10 10:33:41 +0000

    Search Pagination

--- web/app/themes/brookhouse/search.php
+++ web/app/themes/brookhouse/search.php
@@ -15,7 +15,7 @@ get_header();
 
         <header class="page-header">
             <h1 class="page-title">
-                <?php _e( 'Search results for: ', 'brookhouse' ); ?>
+                <?php _e('Search results for: ', 'brookhouse'); ?>
                 <span class="page-description"><?php echo get_search_query(); ?></span>
             </h1>
         </header><!-- .page-header -->
@@ -24,12 +24,12 @@ get_header();
             <?php get_search_form(); ?>
         </div>
 
-        <?php if ( have_posts() ) : ?>
+        <?php if (have_posts()) : ?>
 
-        <?php
-			// Start the Loop.
-			while ( have_posts() ) :
-				the_post();?>
+            <?php
+            // Start the Loop.
+            while (have_posts()) :
+                the_post(); ?>
 
                 <article <?php post_class(); ?>>
                     <?php
@@ -40,33 +40,64 @@ get_header();
                         $document_upload = get_field('document_upload');
                         $ext = pathinfo($document_upload['url'], PATHINFO_EXTENSION);
                         $result_title .= ' (' . $ext . ')';
-                    }
-                    else {
+                    } else {
                         $result_url = get_permalink();
                     }
                     ?>
                     <header>
-                        <h2 class="entry-title"><a href="<?php echo $result_url; ?>"><?php echo $result_title; ?></a></h2>
+                        <h2 class="entry-title"><a href="<?php echo $result_url; ?>"><?php echo $result_title; ?></a>
+                        </h2>
                     </header>
                     <div class="entry-summary">
                         <?php the_excerpt(); ?>
                     </div>
                 </article>
 
-                <?php
+            <?php
 
 
-			endwhile;
+            endwhile;
 
-            the_posts_navigation();
+            global $wp_query;
+
+            $big = 999999999; // need an unlikely integer
+            echo paginate_links(array(
+                'base' => str_replace($big, '%#%', get_pagenum_link($big)),
+                'format' => '?paged=%#%',
+                'mid_size' => 2,
+                'current' => max(1, get_query_var('paged')),
+                'total' => $wp_query->max_num_pages,
+                'prev_text' => '<span class="screen-reader-text">' . __(
+                        'Search results - previous page',
+                        'justicejobs'
+                    ) . '</span><span aria-hidden="true">' . __('PREV', 'justicejobs') . '</span>',
+                'next_text' => '<span class="screen-reader-text"> ' . __(
+                        'Search results',
+                        'justicejobs'
+                    ) . ' -  </span>' . __(
+                        'NEXT',
+                        'justicejobs'
+                    ) . ' <span class="screen-reader-text">' . __(
+                        'page',
+                        'justicejobs'
+                    ) . '</span>',
+                'before_page_number' => '<span class="screen-reader-text">' . __(
+                        'Search results - page',
+                        'justicejobs'
+                    ) . '</span>',
+                'after_page_number' => '<span class="screen-reader-text"> ' . __(
+                        ' of ',
+                        'justicejobs'
+                    ) . __($wp_query->max_num_pages) . '</span>'
+            ));
 
 
         else :
             echo "No Results found";
 
-		endif;
-		?>
+        endif;
+        ?>
     </main><!-- #main -->
-    </div><!-- #primary -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
