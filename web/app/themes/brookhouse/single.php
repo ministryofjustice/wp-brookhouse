f4f0ba005226d3ac1c02679aabde45b20bfd55dc
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2020-03-17 14:32:17 +0000

    Single Article template

--- web/app/themes/brookhouse/single.php
+++ web/app/themes/brookhouse/single.php
@@ -5,7 +5,9 @@
  * @package brookhouse
  */
 
-get_header(); ?>
+get_header();
+get_sidebar();
+?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
@@ -13,14 +15,6 @@ get_header(); ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'content', 'single' ); ?>
-
-			<?php brookhouse_content_nav( 'nav-below' ); ?>
-
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || '0' != get_comments_number() )
-					comments_template();
-			?>
 
 		<?php endwhile; // end of the loop. ?>
 
