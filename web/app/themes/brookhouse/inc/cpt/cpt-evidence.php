acb7edd143fd1194476830d80a56953c540662aa
Beverley Newing <bnewing92@hotmail.co.uk>
2020-03-30 15:11:43 +0100

    Add Evidence Custom Post type base and include it in functions file

---
+++ web/app/themes/brookhouse/inc/cpt/cpt-evidence.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Brookhouse custom post type 'Evidence'
+ *
+ * @package brookhouse
+ */
+
+/**
+*
+* Register the Evidence custom post type
+*
+* */
+add_action('init', 'brookhouse_ctp_evidence_init');
+
+function brookhouse_ctp_document_init()
+{
+    global $brookhouseTaxonomies;
+
+    $labels = [
+        'name' => 'Evidence',
+        'singular_name' => 'Evidence',
+        'add_new' => 'Add New',
+        'add_new_item' => 'Add New',
+        'edit_item' => 'Edit Evidence Entry',
+        'new_item' => 'New Evidence Entry',
+        'all_items' => 'View All Evidence',
+        'view_item' => 'View Evidence Entries',
+        'search_items' => 'Search Evidence',
+        'not_found' => 'No entries found',
+        'not_found_in_trash' => 'No Evidence entries found in Bin',
+        'parent_item_colon' => '',
+        'menu_name' => 'Evidence'
+    ];
+
+    $args = [
+        'labels' => $labels,
+        'public' => true,
+        'publicly_queryable' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'query_var' => true,
+        'rewrite' => array('slug' => 'evidence'),
+        'capability_type' => 'post',
+        'has_archive' => false,
+        'hierarchical' => false,
+        'menu_position' => null,
+        'menu_icon' => 'dashicons-playlist-audio',
+        'supports' => array('title'),
+        'taxonomies' => $brookhouseTaxonomies
+
+    ];
+
+    register_post_type('evidence', $args);
+}
