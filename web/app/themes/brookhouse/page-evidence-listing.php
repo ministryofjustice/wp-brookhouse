b4c871f454be7a86ff6c0a2151fa22b1f65f7c8d
Beverley Newing <bnewing92@hotmail.co.uk>
2020-03-31 16:54:58 +0100

    Add basic display of ACF values

--- web/app/themes/brookhouse/page-evidence-listing.php
+++ web/app/themes/brookhouse/page-evidence-listing.php
@@ -12,124 +12,29 @@ get_header();
         <?php while (have_posts()) : the_post(); ?>
 
             <h1><?php the_title(); ?></h1>
-
             <?php the_content(); ?>
 
-            <?php
-
-            $doc_type = get_field('listing_document_type');
-
-            if (!empty($doc_type)) { ?>
-
-
                 <?php
+                    $query = new WP_Query( array( 'post_type' => 'evidence', 'paged' => $paged ) );
 
-                if (taxonomy_exists($doc_type)) {
-                    $doc_categories = get_terms(array(
-                        'taxonomy' => $doc_type
-                    ));
+                    if ( $query->have_posts() ) : ?>
+                    <?php while ( $query->have_posts() ) : $query->the_post(); ?>
 
-                    if (is_array($doc_categories) && !empty($doc_categories)) { ?>
-                        <div class="doc-listing-filters">
-                            <h2>Evidence List Filters</h2>
-                            <div class="doc-listing-cat-filter-wrapper">
-                                <label for="doc-cat-filter">Evidence Category</label>
-                                <select id="doc-cat-filter">
-                                    <option value="">All categories</option>
-                                    <?php foreach ($doc_categories as $category) { ?>
-                                        <option value="<?php echo $category->term_id; ?>"><?php echo $category->name; ?></option>
-                                        <?php
-                                    }
-                                    ?>
-                                </select>
-                            </div>
+                        <div class="entry">
+                            <h2 class="title"><?php the_title(); ?></h2>
+                            <p>Evidence type: <?php the_field( "evidence_type_to_upload" ); ?></p>
+                            <p>Evidence format: <?php the_field( "evidence_format" ); ?> </p>
+                            <p>Witness type: <?php the_field( "evidence_witness_type" ); ?></p>
+                            <p>Evidence publication date: <?php the_field( "evidence_publish_date" ); ?></p>
+                            <button>View file: <?php the_field( "evidence_upload" ); ?></button>
                         </div>
-                        <div class="document-list">
-                            <?php
 
+                    <?php endwhile; wp_reset_postdata(); ?>
+                    <!-- show pagination here -->
+                <?php else : ?>
+                    <!-- show 404 error here -->
+            <?php endif; ?>
 
-                            foreach ($doc_categories as $category) { ?>
-                                <?php
-
-                                $documents = get_posts(
-                                    array(
-                                        'post_type' => 'documents',
-                                        'posts_per_page' => -1,
-                                        'tax_query' => array(
-                                            array(
-                                                'taxonomy' => $doc_type,
-                                                'field' => 'slug',
-                                                'terms' => $category->slug,
-                                            ),
-                                        ),
-                                        'orderby' => 'meta_value_num',
-                                        'meta_key' => 'publish_date',
-                                        'order' => 'DESC',
-                                    )
-                                );
-
-                                if (is_array($documents) && !empty($documents)) { ?>
-                                    <div id="doc-cat-<?php echo $category->term_id; ?>"
-                                         class="document-list-by-category">
-                                        <h2><?php echo $category->name; ?></h2>
-
-                                        <?php
-
-                                        foreach ($documents as $doc) {
-                                            include(locate_template('content-document-list-item.php', false, false));
-                                        }
-                                        ?>
-                                    </div>
-                                    <?php
-
-                                }
-
-                            }
-                            ?>
-                        </div>
-                        <?php
-
-                    } else {
-                        $documents = get_posts(
-                            array(
-                                'post_type' => 'evidence',
-                                'posts_per_page' => -1,
-                                'meta_query' => array(
-                                    array(
-                                        'key' => 'document_type_to_upload',
-                                        'value' => $doc_type,
-                                    ),
-                                ),
-                                'orderby' => 'meta_value_num',
-                                'meta_key' => 'evidence_publish_date',
-                                'order' => 'DESC',
-                            )
-                        );
-
-                        if (is_array($documents) && !empty($documents)) { ?>
-
-                            <div class="document-list">
-
-                                <?php
-                                foreach ($documents as $doc) {
-
-                                    include(locate_template('content-document-list-item.php', false, false));
-                                }
-                                ?>
-                            </div>
-                            <?php
-
-                        } else { ?>
-                            <p>No Documents Found</p>
-                            <?php
-
-                        }
-                    }
-                }
-
-
-            }
-            ?>
         <?php endwhile; // end of the loop. ?>
     </main>
 </div>
