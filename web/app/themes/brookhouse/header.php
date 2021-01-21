32addd16042dacaa83d37697c93c948b23829143
Beverley Newing <bnewing92@hotmail.co.uk>
2021-01-21 16:56:42 +0000

    Add cookie banner 2.0.1 to Brook House

--- web/app/themes/brookhouse/header.php
+++ web/app/themes/brookhouse/header.php
@@ -30,7 +30,19 @@ $moj_bh_header_link = get_field('header_link', 'option');
 
 <body <?php body_class('locale-' . get_locale()); ?>>
 
-    <?php
+<?php
+if ( ! function_exists( 'wp_body_open' ) ) {
+	/**
+	 * Open the body tag, pull in any hooked triggers.
+	 **/
+	function wp_body_open() {
+		do_action( 'wp_body_open' );
+	}
+}
+wp_body_open();
+?>
+
+<?php
 
 /**
  * MoJ hook in our themes to allow content to be hooked and placed
