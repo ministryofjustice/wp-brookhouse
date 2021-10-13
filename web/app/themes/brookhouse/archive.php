05073c2405d6b2c56aee083efa9e75e9a32cf811
adam <brown.a2@gmail.com>
2021-10-13 21:33:10 +0100

    Add search filter for categories and modify archive page

--- web/app/themes/brookhouse/archive.php
+++ web/app/themes/brookhouse/archive.php
@@ -19,7 +19,8 @@ get_header(); ?>
 				<h1 class="page-title">
 					<?php
 						if ( is_category() ) :
-							single_cat_title();
+							echo 'All pages in the topic: ';
+                            single_cat_title();
 
 						elseif ( is_tag() ) :
 							single_tag_title();
@@ -78,12 +79,35 @@ get_header(); ?>
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
+            <h2 class="entry-title">
+                <a href="<?php echo get_permalink(get_the_ID()); ?>">
+            <?php the_title(); ?></a>
+            </h2>
+            <br>
+
+
 				<?php
 					/* Include the Post-Format-specific template for the content.
 					 * If you want to override this in a child theme, then include a file
 					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 					 */
-					get_template_part( 'content', get_post_format() );
+                    // echo '<a href="';
+
+                    // get_permalink(get_the_ID());
+
+                    // echo '">';
+
+
+
+                    // echo '</a>';
+
+                    the_excerpt();
+                    the_category(', ');
+
+                    echo '<br>';
+                    echo '<hr>';
+                    echo '<br>';
+					//get_template_part( 'content', get_post_format() );
 				?>
 
 			<?php endwhile; ?>
--- web/app/themes/brookhouse/searchform.php
+++ web/app/themes/brookhouse/searchform.php
@@ -4,11 +4,41 @@
  *
  * @package brookhouse
  */
+
+ // Create conditional that displays a special search form on the search page.
+ if (!is_search()) :
 ?>
+
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
-		<span class="screen-reader-text"><?php _ex( 'Search for:', 'label', 'brookhouse' ); ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'brookhouse' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
+		<span class="screen-reader-text"><?php _e( 'Search for:', 'label', 'brookhouse' ); ?></span>
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'brookhouse' ); ?>"
+        value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
 	</label>
 	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'brookhouse' ); ?>">
 </form>
+
+<?php
+
+else :
+
+?>
+
+
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<label>
+		<span class="screen-reader-text"><?php _e( 'Search for:', 'label', 'brookhouse' ); ?></span>
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'brookhouse' ); ?>"
+        value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
+	</label>
+
+    <button type="submit" class="search-submit btn btn-default" value="<?php echo esc_attr_x( 'Search', 'submit button', 'brookhouse' ); ?>">
+    <?php _e( 'Search', 'brookhouse' ); ?>
+    </button>
+
+</form>
+
+
+<?php
+
+endif;
