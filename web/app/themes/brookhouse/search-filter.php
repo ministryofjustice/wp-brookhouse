3edf773709dbe769a6f7bc73b0b5eb7d1c8cedbf
adam <brown.a2@gmail.com>
2021-10-15 10:56:00 +0100

    Update archive page to support category returned results
    Modify the archive page so that it returns a list of posts with a short
    excerpt and category linked tags under those excerpts. Tweaks to search
    landing page including to the search fields that determine how you
    filter content. These were made after consulting the team. It was
    determined that these changes were best as MVP to roll out.

--- web/app/themes/brookhouse/search-filter.php
+++ web/app/themes/brookhouse/search-filter.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying search forms in brookhouse
  *
@@ -6,38 +7,47 @@
  */
 
 // Get the current selected category so the correct dropdown is selected
-$cat = ( is_category() ) ? get_query_var( 'cat' ) : 0;
+$cat = ( is_category() ) ? get_query_var('cat') : 0;
 ?>
 
-<form id="formfilter" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form id="formfilter" role="search" method="get" action="<?php echo esc_url(home_url('/')); ?>">
 
   <div class="input-group" >
 
-    <label class="sr-only"><?php _e( 'Filter results by topic:', 'brookhouse' ); ?></label>
-    <br><br>
-    <div class="input-group-dropdown">
-		<?php
-		wp_dropdown_categories(
-			[
-				'show_option_all' => 'All topics',
-				'orderby'         => 'name',
-				'echo'            => 1,
-				'selected'        => $cat,
-				'hierarchical'    => true,
-				'query_var'       => true,
-				'class'           => 'dropdown-menu',
-				'id'              => 'custom-cat-drop',
-				'value_field'     => 'term_id',
-			]
-		);
-		?>
+    <label class="sr-only"><?php _e('Filter search results by topic:', 'brookhouse'); ?></label>
 
-    <span class="input-group-btn">
-	  <button type="submit" class="search-submit btn btn-default">
-    <?php _e( 'Filter', 'brookhouse' ); ?></button>
-	<br></span>
+    <br><br>
+
+    <div class="input-group-dropdown">
+        <?php
+        wp_dropdown_categories(
+            [
+                'show_option_all' => 'All topics',
+                'orderby'         => 'name',
+                'echo'            => 1,
+                'selected'        => $cat,
+                'hierarchical'    => true,
+                'query_var'       => true,
+                'class'           => 'dropdown-menu',
+                'id'              => 'custom-cat-drop',
+                'value_field'     => 'term_id'
+            ]
+        );
+        ?>
+
     </div>
 
+    <label style="visibility:hidden; position:absolute;"><?php _e('Search:', 'brookhouse'); ?></label>
+
+    <input type="search" value="<?php echo get_search_query(); ?>"
+    name="s"
+    class="search-field form-control input-group-dropdown"
+    placeholder="<?php _e('Search by keyword', 'brookhouse'); ?>">
+
+    <span class="input-group-btn">
+      <button type="submit" class="search-submit btn btn-default"><?php _e('Search', 'brookhouse'); ?></button>
+    <br>
+    </span>
   </div>
 </form>
 
