6d9641eda7cf679b107729f034d97f12c5cdd418
adam <brown.a2@gmail.com>
2021-10-15 11:16:38 +0100

    Remove unused commented out code

--- web/app/themes/brookhouse/archive.php
+++ web/app/themes/brookhouse/archive.php
@@ -81,31 +81,13 @@ get_header(); ?>
             </h2>
             <br>
 
-
-                <?php
-                    /* Include the Post-Format-specific template for the content.
-                     * If you want to override this in a child theme, then include a file
-                     * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                     */
-                    // echo '<a href="';
-
-                    // get_permalink(get_the_ID());
-
-                    // echo '">';
-
-
-
-                    // echo '</a>';
-
-                    the_excerpt();
-                    the_category(', ');
-
-                    echo '<br>';
-                    echo '<hr>';
-                    echo '<br>';
-                    //get_template_part( 'content', get_post_format() );
-                ?>
-
+            <?php
+                the_excerpt();
+                the_category(', ');
+            ?>
+            <br>
+            <hr>
+            <br>
             <?php endwhile; ?>
 
             <?php brookhouse_content_nav('nav-below'); ?>
